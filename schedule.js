eaad69fe9819ae44b586c9a1ef1f126efb883fdc
whyhugo <hugo.tw.wang@gmail.com>
2024-12-02 04:41:11 +0800

    refactor: update schedule with new course entries and correct formatting

--- schedule.js
+++ schedule.js
@@ -2,31 +2,46 @@
 const schedules = {
     Monday: `
         <ul>
-            <li>08:10 - 09:00: Linear Algebra (理圖003)</li>
-            <li>09:10 - 10:00: Excel for Data Analysis (系所自排)</li>
+            <li>09:10 - 10:00: Linear Algebra (理圖 003)</li>
+            <li>10:20 - 11:10: Linear Algebra (理圖 003)</li>
+            <li>11:20 - 12:10: Linear Algebra (理圖 003)</li>
+            <li>13:20 - 14:10: Data Processing with Excel (NTU)</li>
+            <li>14:20 - 15:10: Data Processing with Excel (NTU)</li>
+            <li>17:30 - 18:20: Web Computing and XML (B103)</li>
+            <li>18:40 - 19:30: Web Computing and XML (B103)</li>
+            <li>19:35 - 20:25: Web Computing and XML (B103)</li>
         </ul>
     `,
     Tuesday: `
         <ul>
-            <li>10:20 - 11:10: Network Computing and XML (B103)</li>
-            <li>11:20 - 12:10: Network Computing and XML (B103)</li>
+            <li>09:10 - 10:00: Computer Programming I (理圖 807)</li>
+            <li>10:20 - 11:10: Computer Programming I (理圖 807)</li>
+            <li>11:20 - 12:10: Computer Programming I (理圖 807)</li>
+            <li>15:30 - 16:20: Introduction to Philosophy of Biology (B101)</li>
+            <li>16:30 - 17:20: Introduction to Philosophy of Biology (B101)</li>
         </ul>
     `,
     Wednesday: `
         <ul>
-            <li>10:20 - 11:10: Network Computing and XML (B103)</li>
-            <li>11:20 - 12:10: Linear Algebra (理圖003)</li>
+            <li>13:20 - 14:10: Introduction to Management (誠 302)</li>
+            <li>14:20 - 15:10: Introduction to Management (誠 302)</li>
         </ul>
     `,
     Thursday: `
         <ul>
-            <li>09:10 - 10:00: Linear Algebra (理圖003)</li>
-            <li>10:20 - 11:10: Excel for Data Analysis (系所自排)</li>
+            <li>13:20 - 14:10: Calculus B I (E201)</li>
+            <li>14:20 - 15:10: Calculus B I (E201)</li>
+            <li>15:30 - 16:20: Calculus B I (E201)</li>
+            <li>16:30 - 17:20: Principals and Practical Applications of Color Scheme (NTUST)</li>
+            <li>17:30 - 18:20: Principals and Practical Applications of Color Scheme (NTUST)</li>
         </ul>
     `,
     Friday: `
         <ul>
-            <li>09:10 - 10:00: Excel for Data Analysis (系所自排)</li>
+            <li>10:20 - 11:10: English III (S602)</li>
+            <li>11:20 - 12:10: English III (S602)</li>
+            <li>15:30 - 16:20: Beginning Badminton (體育館一 4F)</li>
+            <li>16:30 - 17:20: Beginning Badminton (體育館一 4F)</li>
         </ul>
     `,
     Saturday: `
