41ab6949684fc64275e3cc8eb09943029e4eb42b
whyhugo <hugo.tw.wang@gmail.com>
2024-12-02 20:16:21 +0800

    style: add AOS animation classes to various sections for improved visual effects

--- script.js
+++ script.js
@@ -7,7 +7,24 @@ function myFunction() {
     }
 }
 
+//Animate on scroll
+function isElementInViewport(el) {
+  const rect = el.getBoundingClientRect();
+  return rect.bottom < 0 || rect.top > window.innerHeight;
+}
 
+function addClassToVisibleElements() {
+  var aosElements = document.querySelectorAll(".aos");
+  aosElements.forEach(function (aosElement) {
+    if (!isElementInViewport(aosElement)) aosElement.classList.add("ed");
+    else aosElement.classList.remove("ed");
+  });
+}
+
+document.addEventListener("scroll", addClassToVisibleElements);
+addClassToVisibleElements();
+
+// Load resume from XML file
 document.addEventListener('DOMContentLoaded', loadResume);
 
 function loadResume() {
